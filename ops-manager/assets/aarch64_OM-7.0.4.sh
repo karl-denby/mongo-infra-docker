ecd77c03392283cd2833c15e4141735c65ed00e5
karl-denby <karl.denby@gmail.com>
2024-04-15 21:28:08 +0100

    another blind attempt at aarch64 support

---
+++ ops-manager/assets/aarch64_OM-7.0.4.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl -o mongodb-enterprise.aarch64.rpm -L https://repo.mongodb.com/yum/redhat/8/mongodb-enterprise/7.0/aarch64/RPMS/mongodb-enterprise-server-7.0.8-1.el8.aarch64.rpm
+curl -o mongodb-mms.x86_64.rpm -L https://downloads.mongodb.com/on-prem-mms/rpm/mongodb-mms-7.0.4.500.20240405T1431Z.x86_64.rpm 
