1bdd9b89ead1da80da06a9046679163025d85162
Karl Denby <karl.denby@gmail.com>
2024-04-18 11:51:09 +0100

    make certs for cm wildcard

--- cloud-manager/certs/create-certificates.sh
+++ cloud-manager/certs/create-certificates.sh
@@ -4,25 +4,11 @@ openssl req -new -x509 -days 1826 -key mongodb-ca.key -out mongodb-ca.crt -confi
 cat mongodb-ca.crt mongodb-ca.key > mongodb-ca.pem
 
 #n1
-openssl genrsa -out n1cm.cm.internal.key 4096
-openssl req -new -key n1cm.cm.internal.key -out n1cm.cm.internal.csr -config conf/openssl-n1cm.cnf -subj "/C=IE/ST=Leinster/L=Dublin/O=Support Example/OU=Customer Engineering/CN=cm.internal"
-openssl x509 -req -days 365 -in n1cm.cm.internal.csr -CA mongodb-ca.crt -CAkey mongodb-ca.key -CAcreateserial -out n1cm.cm.internal.crt -extfile conf/openssl-n1cm.cnf -extensions v3_req
-cat n1cm.cm.internal.crt n1cm.cm.internal.key > n1cm.cm.internal.pem
-
-#n2
-openssl genrsa -out n2cm.cm.internal.key 4096
-openssl req -new -key n2cm.cm.internal.key -out n2cm.cm.internal.csr -config conf/openssl-n2cm.cnf -subj "/C=IE/ST=Leinster/L=Dublin/O=Support Example/OU=Customer Engineering/CN=cm.internal"
-openssl x509 -req -days 365 -in n2cm.cm.internal.csr -CA mongodb-ca.crt -CAkey mongodb-ca.key -CAcreateserial -out n2cm.cm.internal.crt -extfile conf/openssl-n2cm.cnf -extensions v3_req
-cat n2cm.cm.internal.crt n2cm.cm.internal.key > n2cm.cm.internal.pem
-
-#n3
-openssl genrsa -out n3cm.cm.internal.key 4096
-openssl req -new -key n3cm.cm.internal.key -out n3cm.cm.internal.csr -config conf/openssl-n3cm.cnf -subj "/C=IE/ST=Leinster/L=Dublin/O=Support Example/OU=Customer Engineering/CN=cm.internal"
-openssl x509 -req -days 365 -in n3cm.cm.internal.csr -CA mongodb-ca.crt -CAkey mongodb-ca.key -CAcreateserial -out n3cm.cm.internal.crt -extfile conf/openssl-n3cm.cnf -extensions v3_req
-cat n3cm.cm.internal.crt n3cm.cm.internal.key > n3cm.cm.internal.pem
+openssl genrsa -out servers.cm.internal.key 4096
+openssl req -new -key servers.cm.internal.key -out servers.cm.internal.csr -config conf/openssl-servers.cnf -subj "/C=IE/ST=Leinster/L=Dublin/O=Support Example/OU=Customer Engineering/CN=cm.internal"
+openssl x509 -req -days 365 -in servers.cm.internal.csr -CA mongodb-ca.crt -CAkey mongodb-ca.key -CAcreateserial -out servers.cm.internal.crt -extfile conf/openssl-servers.cnf -extensions v3_req
+cat servers.cm.internal.crt servers.cm.internal.key > servers.cm.internal.pem
 
 #tidy
 rm mongodb-ca.crt mongodb-ca.srl
-rm n1cm.cm.internal.crt n1cm.cm.internal.key n1cm.cm.internal.csr
-rm n2cm.cm.internal.crt n2cm.cm.internal.key n2cm.cm.internal.csr
-rm n3cm.cm.internal.crt n3cm.cm.internal.key n3cm.cm.internal.csr
+rm servers.cm.internal.crt servers.cm.internal.key servers.cm.internal.csr
