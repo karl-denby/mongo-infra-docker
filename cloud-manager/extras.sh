c96a5a94286eb771434d681abfba1be9e4a642fc
Dan Maor <maor.dan@gmail.com>
2024-07-02 16:49:31 +0300

    fixed extras.sh

--- cloud-manager/extras.sh
+++ cloud-manager/extras.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo Please choose some extras: 
-platform_options=("pause" "un-pause" "more-servers" "even-more-servers" "Quit")
+platform_options=("pause" "un-pause" "more-servers" "stop-more-servers" "even-more-servers" "stop-even-more-servers" "Quit")
 select opt in "${platform_options[@]}"
 do
   case $opt in
@@ -19,9 +19,19 @@ do
       docker compose up -d n2cm n3cm
       break
       ;;
+    stop-more-servers)
+      echo "Starting n2cm and n3cm"
+      docker compose down n2cm n3cm
+      break
+      ;;
     even-more-servers)
       echo "Starting n4cm, n5cm & n6cm"
       docker compose up -d n4cm n5cm n6cm
+      break
+      ;;
+    stop-even-more-servers)
+      echo "Starting n4cm, n5cm & n6cm"
+      docker compose down n4cm n5cm n6cm
       break
       ;;
     Quit)
